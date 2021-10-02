6fc7604415ed891ef201af8f0c1f86a268eacd5c
QuinnG <quinngilbert12and3@gmail.com>
2021-10-01 23:14:52 -0400

    Solution24 Skeleton

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+
+public class Solution24 {
+
+    public static void main(String args[]){
+
+        //take input
+        //check if anagram and print output
+
+    }
+
+    static boolean isAnagram(String a, String b){
+        //change strings to char[] so that they can be sorted easily
+        //sort the strings so that the arrays will be equal if they are anagrams
+        //return whether the arrays are equal
+    }
+
+}
