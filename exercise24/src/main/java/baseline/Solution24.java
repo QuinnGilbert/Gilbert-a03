46c67b8a811fb3071b3adb302e7230f53ebb0220
QuinnG <quinngilbert12and3@gmail.com>
2021-10-01 23:43:20 -0400

    Solution24

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -7,17 +7,31 @@ import java.util.*;
 
 public class Solution24 {
 
-    public static void main(String args[]){
-
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
         //take input
+        System.out.print("Enter the first string: ");
+        String string1 = in.next();
+        System.out.print("Enter the second string: ");
+        String string2 = in.next();
         //check if anagram and print output
-
+        System.out.printf("\"%s\" and \"%s\" are ",string1,string2);
+        if(isAnagram(string1,string2)){
+            System.out.println("anagrams");
+        }else{
+            System.out.println("not anagrams");
+        }
     }
 
-    static boolean isAnagram(String a, String b){
+    public static boolean isAnagram(String a, String b){
         //change strings to char[] so that they can be sorted easily
+        char[] arrayA = a.toCharArray();
+        char[] arrayB = b.toCharArray();
         //sort the strings so that the arrays will be equal if they are anagrams
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
         //return whether the arrays are equal
+        return Arrays.toString(arrayA).equals(Arrays.toString(arrayB));
     }
 
 }
---
+++ exercise24/src/test/java/baseline/Solution24Test.java
@@ -0,0 +1,13 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution24Test {
+    @Test
+    public void testAnagram(){
+        assertTrue(Solution24.isAnagram("note","tone"));
+        assertFalse(Solution24.isAnagram("other","rather"));
+    }
+}
