ce42ee3f73fa148e2224b00f220f927cf286cbcb
QuinnG <quinngilbert12and3@gmail.com>
2021-10-02 21:39:17 -0400

    Solution 25 skeleton

---
+++ exercise24-challenge01/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+Y
+)src/main/java/challenges/Challenge01.java,b\e\be9f66d176a0789d791e767cdce39f9244ac4805
+]
+-src/test/java/challenges/Challenge01Test.java,a\5\a5f145538dc7991a81d04a5106cff26c05d53683
---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import java.util.*;
+public class Solution25 {
+    public static void main(String[] args) {
+        //input password
+        //get value from passwordValidator
+        //interpret value
+        //print result
+
+    }
+
+    static int passwordValidator(String s){
+        //count letters, numbers and special
+        //return 0 for weak and 3 for very strong
+        //return -1 for other
+    }
+
+}
