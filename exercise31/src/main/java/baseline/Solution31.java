a8bf849337992738a54685d576e23e30c69bf6ae
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 21:28:57 -0400

    added 31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -8,8 +8,19 @@ public class Solution31 {
 
     public static void main(String[] args) {
         //input age and resting heart rate
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = in.nextInt();
+        System.out.print("Enter resting heart rate: ");
+        int resting = in.nextInt();
         //loop through intensities
         //Target = (((220 − age) − restingHR) × intensity) + restingHR
+        System.out.println("Intensity| Rate");
+        System.out.println("---------------");
+        for(int i=55;i<=95;i++){
+            double BPM = ((220-age)-resting)*i/100.0 + resting;
+            System.out.println(i+"%      | "+BPM+" bpm");
+        }
     }
 
 }
