f0e3d115e27a627158cae66941684e90cebf5b39
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 20:42:27 -0400

    28 skeleton

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import java.util.*;
+public class Solution28 {
+    public static void main(String[] args) {
+        //create variable to store sum
+        //create loop to take input
+        //print result
+    }
+}
