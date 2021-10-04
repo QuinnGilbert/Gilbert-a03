9827625c13dfdee541fea75e19c49ac0aba82b75
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 20:17:27 -0400

    27 skeleton

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import java.util.*;
+public class Solution27 {
+
+    public static void main(String[] args) {
+        //input values
+
+        //call validation function with values
+    }
+
+    static void validateInput(){
+        //call each function and print appropriate output
+    }
+    static boolean checkFirstName(String first){
+        //return true if string is not empty
+
+    }
+    static boolean checkLastName(String last){
+        //return true if string is not empty
+
+    }
+    static boolean checkFirstNameLength(String first){
+        //return true if longer than 2
+    }
+    static boolean checkLastNameLength(String last){
+        //return true if longer than 2
+    }
+    static boolean checkID(String ID){
+        //return true if in format AA-XXXX
+    }
+    static boolean checkZIP(String zip){
+        //return true if zip is a number
+    }
+
+
+}
