50a6659965757c97a1f715f3eafe2f3e9e45d640
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 21:01:02 -0400

    added 29

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -7,7 +7,26 @@ import java.util.*;
 public class Solution29 {
     public static void main(String[] args) {
         //use while loop to keep taking input until correct
-        //check if input is a number
-        //check if input is not 0
+        Scanner in = new Scanner(System.in);
+        boolean check = true;
+        int r = 0;
+        do{
+            if(!check){
+                System.out.println("Sorry. That's not a valid input.");
+                check = true;
+            }
+            System.out.print("What's the rate of return: ");
+            String input = in.next();
+            //check if input is number
+            for(char i:input.toCharArray()){
+                if(i<'0'||i>'9'){
+                    check = false;
+                }
+            }
+            if(!check)continue;
+            r = Integer.parseInt(input);
+            if(r==0)check = false;
+        }while(!check);
+        System.out.println("It will take "+72/r+" years for your investment to double.");
     }
 }
