c4f954d2190f7ed43711437b9c989c422966a95e
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 21:17:24 -0400

    31 skeleton

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import java.util.*;
+public class Solution31 {
+
+    public static void main(String[] args) {
+        //input age and resting heart rate
+        //loop through intensities
+        //Target = (((220 − age) − restingHR) × intensity) + restingHR
+    }
+
+}
