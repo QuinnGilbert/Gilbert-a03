095bed5661d73155134e98608a9c2a60189a61af
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 20:43:57 -0400

    added 28

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -7,7 +7,15 @@ import java.util.*;
 public class Solution28 {
     public static void main(String[] args) {
         //create variable to store sum
+        int sum=0;
         //create loop to take input
+        Scanner in = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            System.out.print("Input a number: ");
+            sum+=in.nextInt();
+        }
         //print result
+        System.out.println(sum);
+
     }
 }
