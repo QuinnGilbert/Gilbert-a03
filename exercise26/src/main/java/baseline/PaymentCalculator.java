078760f4272fdd5d0690f5d65132787cf3b0a700
QuinnG <quinngilbert12and3@gmail.com>
2021-10-03 20:01:05 -0400

    Added 26

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -14,5 +14,10 @@ public class PaymentCalculator {
     //n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
     public int  calculateMonthsUntilPaidOff(){
         //use function to calculate months
+        //round up fractions of a cent
+        double dailyAPR = Math.ceil(APR/365*100)/100.0;
+        double months = -(1/30.0) * (Math.log(1+balance/monthlyPayment*(1-Math.pow((1+dailyAPR),30)))/Math.log(1+dailyAPR));
+        //round up to nearest month
+        return (int)Math.ceil(months);
     }
 }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -7,8 +7,18 @@ import java.util.*;
 public class Solution26 {
     public static void main(String[] args) {
         //input values
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your balance? ");
+        double balance = in.nextDouble();
+        System.out.print("What is the APR on the card (as a percent)?");
+        double APR = in.nextDouble()/100;
+        System.out.print("What is the monthly payment you can make?");
+        double payment = in.nextDouble();
         //create PaymentCalculator with values
+        PaymentCalculator calculator = new PaymentCalculator(balance,APR,payment);
         //get number of months
+        int months = calculator.calculateMonthsUntilPaidOff();
         //print result
+        System.out.println("It will take "+months+" months to pay off this card.");
     }
 }
