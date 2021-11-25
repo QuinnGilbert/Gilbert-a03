e38fbb50c651b8afc43a236bc2e560e9e2a57905
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 17:39:32 -0500

    added exercise30 unit test

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,20 +1,26 @@
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Quinn Gilbert
  */
 package baseline;
 
 public class Solution30 {
 
     public static void main(String[] args) {
+        System.out.println(genTable());
+    }
+
+    static String genTable(){
         //create double for loop
-        //use printf to space output
+        //use String.format to space output
+        StringBuilder table = new StringBuilder();
         for(int i=1;i<=12;i++){
             for(int j=1;j<=12;j++){
-                System.out.printf("%4d ",i*j);
+                table.append(String.format("%4d ",i*j));
             }
-            System.out.println();
+            table.append("\n");
         }
+        return table.toString();
     }
 
 }
