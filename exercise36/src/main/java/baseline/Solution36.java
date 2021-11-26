90a5e260316faedd31c1c74acaa46bfc6bba899d
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 20:06:35 -0500

    added 36 skeleton

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+public class Solution36 {
+
+    public static void main(String[] args) {
+        //keep entering numbers and adding them to list until done
+        //get and print average, min, max, and std dev
+    }
+
+    static int getMax(ArrayList<Integer> list){
+
+    }
+
+    static int getMin(ArrayList<Integer> list){
+
+    }
+
+    static int getAverage(ArrayList<Integer> list){
+
+    }
+
+    static int getStdDev(ArrayList<Integer> list){
+
+    }
+}
---
+++ exercise36/src/test/java/baseline/Solution36Test.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution36Test {
+
+}
