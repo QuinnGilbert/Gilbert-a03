4534c537e638bd981754f53ec75488545cc0afda
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 20:47:40 -0500

    37 full

--- exercise37/src/main/java/baseline/Solution37.java
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -7,10 +7,51 @@ import java.util.*;
 public class Solution37 {
     public static void main(String[] args) {
         //prompt and input minlength, special chars, numbers
+        Scanner in = new Scanner(System.in);
+        System.out.print("What's the min length? ");
+        int minLength = in.nextInt();
+        System.out.print("How many special characters? ");
+        int specialChars = in.nextInt();
+        System.out.print("How many numbers? ");
+        int numbers = in.nextInt();
         //generate random password
+        System.out.println("Your password is "+genPassword(minLength,specialChars,numbers));
     }
 
-    static String genPassword(int minLength, int specialChars, int numbers){
+    static Random random = new Random();
+    static String genPassword(int minLength, int specialChars, int numbers) {
         //randomly get correct number of each value from special list and pad with letters
+        char[] special = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')'};
+        char[] num = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+        char[] letters = new char[52];
+        for(int i=0;i<26;i++) letters[i] = (char)(i+'a');
+        for(int i=0;i<26;i++) letters[i+26] = (char)(i+'A');
+        int length = Math.max(minLength, 2*(specialChars + numbers));
+
+        char[] password = new char[length];
+        //use array list of indexes to get a random place to put each character in string
+        ArrayList<Integer> indexes = new ArrayList<>();
+        for(int i=0;i<length;i++){
+            indexes.add(i);
+        }
+        //add the special characters
+        for(int i=0;i<specialChars;i++){
+            char c = special[random.nextInt(10)];
+            int index = random.nextInt(indexes.size());
+            password[indexes.remove(index)] = c;
+        }
+        for(int i=0;i<specialChars;i++){
+            char c = num[random.nextInt(10)];
+            int index = random.nextInt(indexes.size());
+            password[indexes.remove(index)] = c;
+        }
+        for(int i:indexes){
+            password[i] = letters[random.nextInt(52)];
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<length;i++){
+            result.append(password[i]);
+        }
+        return result.toString();
     }
 }
--- exercise37/src/test/java/baseline/Solution37Test.java
+++ exercise37/src/test/java/baseline/Solution37Test.java
@@ -9,6 +9,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution37Test {
+
     @Test
+    void testGenPassword(){
+        String password = Solution37.genPassword(8,2,2);
+        int specials = 0;
+        int numbers = 0;
+        int letters = 0;
+        for(int i=0;i<password.length();i++){
+            if(password.charAt(i)>='0'&&password.charAt(i)<='9'){
+                numbers++;
+            }else if(password.charAt(i)>='a'&&password.charAt(i)<='z'||password.charAt(i)>='A'&&password.charAt(i)<='Z'){
+                letters++;
+            }else{
+                specials++;
+            }
+        }
+        assertEquals(2,specials);
+        assertEquals(2,numbers);
+        assertEquals(4,letters);
+    }
 
 }
