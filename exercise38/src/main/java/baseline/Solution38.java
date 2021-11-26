5ce22744823387142fcf586194ba7fb716899aac
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 20:56:06 -0500

    38 skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+public class Solution38 {
+
+    public static void main(String[] args) {
+        //prompt and input list of numbers
+        //get list of only evens from input numbers
+    }
+
+    static int[] filterNumbers(int[] arr){
+        //loop through array and add only even numbers
+    }
+}
