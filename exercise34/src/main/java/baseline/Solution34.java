55c84a060e55eea7670b29cdb9ab161a0b1e9ac6
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 19:17:56 -0500

    added skeleton

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+public class Solution34 {
+
+    static ArrayList<String> list;
+    public static void main(String[] args) {
+        //create initial list
+        //prompt and input employee to remove
+        //remove employee
+    }
+
+    static void genList(){
+        //create initial list
+    }
+
+    static void remove(String employee){
+        //remove employee from list
+    }
+
+}
