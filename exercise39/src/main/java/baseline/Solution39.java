fda0c96d806d5ae2c2c0e7b81e39a98847f1bc92
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 21:45:23 -0500

    39 skeleton

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+public class Solution39 {
+
+    public static void main(String[] args){
+        //generate appropriate data
+        //sort printed data
+    }
+
+    void genTable(){
+        //generate data based on table given in problem
+
+    }
+
+    void printSortedData(){
+        //sort data with custom comparator
+        //print data
+    }
+
+}
