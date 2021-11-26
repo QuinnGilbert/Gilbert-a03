1c42ecb084cd20417f4ba1c24a4df53aac568688
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 20:23:40 -0500

    36 full

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -8,22 +8,61 @@ public class Solution36 {
 
     public static void main(String[] args) {
         //keep entering numbers and adding them to list until done
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        while(true){
+            System.out.print("Enter a number: ");
+            String s = in.next();
+            if(s.equals("done"))break;
+            if(isValid(s))list.add(Integer.parseInt(s));
+        }
         //get and print average, min, max, and std dev
+        System.out.println("The average is "+getAverage(list));
+        System.out.println("The maximum is "+getMax(list));
+        System.out.println("The minimum is "+getMin(list));
+        System.out.println("The standard deviation is "+getStdDev(list));
+    }
+
+    static boolean isValid(String s){
+        //test if string is a number
+        try{
+            Integer.parseInt(s);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
     }
 
     static int getMax(ArrayList<Integer> list){
-
+        int max = list.get(0);
+        for(int i:list){
+            max = Math.max(max,i);
+        }
+        return max;
     }
 
     static int getMin(ArrayList<Integer> list){
-
+        int min = list.get(0);
+        for(int i:list){
+            min = Math.min(min,i);
+        }
+        return min;
     }
 
-    static int getAverage(ArrayList<Integer> list){
-
+    static double getAverage(ArrayList<Integer> list){
+        int sum = 0;
+        for(int i:list){
+            sum+=i;
+        }
+        return sum/(double)list.size();
     }
 
-    static int getStdDev(ArrayList<Integer> list){
-
+    static double getStdDev(ArrayList<Integer> list){
+        double mean = getAverage(list);
+        double sum = 0;
+        for(int i:list){
+            sum+=(i-mean)*(i-mean);
+        }
+        return Math.sqrt(sum/list.size());
     }
 }
--- exercise36/src/test/java/baseline/Solution36Test.java
+++ exercise36/src/test/java/baseline/Solution36Test.java
@@ -4,8 +4,29 @@
  */
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution36Test {
+    @Test
+    void testIsValid(){
+        assertTrue(Solution36.isValid("22"));
+        assertFalse(Solution36.isValid("ABC"));
+    }
 
+    @Test
+    void testStatistics(){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(1000);
+        list.add(300);
+        assertEquals(400,Solution36.getAverage(list));
+        assertEquals(1000,Solution36.getMax(list));
+        assertEquals(100,Solution36.getMin(list));
+        assertEquals(Math.sqrt(125000),Solution36.getStdDev(list));
+    }
 }
