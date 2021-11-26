81e2eeff97f1a00869153948b6a6fa5de47f7cdd
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 19:05:26 -0500

    added skeleton

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+public class Solution33 {
+
+    public static void main(String[] args) {
+        //create array
+        //prompt question and wait for input
+        //get random response
+        //output response
+    }
+
+    static String getResponse(){
+        //store responses
+        //get random number
+        //return corresponding response
+    }
+
+}
