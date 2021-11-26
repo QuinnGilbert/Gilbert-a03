eeaa90bfcc792476c7ae4cf5b0d9fc242744cce6
QuinnG <quinngilbert12and3@gmail.com>
2021-11-25 20:30:14 -0500

    37 skeleton

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+import java.util.*;
+public class Solution37 {
+    public static void main(String[] args) {
+        //prompt and input minlength, special chars, numbers
+        //generate random password
+    }
+
+    static String genPassword(int minLength, int specialChars, int numbers){
+        //randomly get correct number of each value from special list and pad with letters
+    }
+}
---
+++ exercise37/src/test/java/baseline/Solution37Test.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Quinn Gilbert
+ */
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution37Test {
+    @Test
+
+}
